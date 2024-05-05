c8f0449818e018a6742d2f92a743d3468031933c
madeline <madgrismad@gmail.com>
2024-05-05 04:07:37 -0700

    first commit

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+// import './topics/01-basic-types'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+Hola mundo
+  
+`
+console.log('Hola mundo')
+
+
---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+const name ="madeline";
+let hpPoints: number | "full" = 100;
+const isAlive: boolean = true;
+
+hpPoints="full";
+
+
+console.log(name, hpPoints, isAlive);
+
+export{};
